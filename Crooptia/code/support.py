568eb54692b6fbc07f64c116cd735cb96f464c0b
razindanish <54845743+razindanish@users.noreply.github.com>
2023-04-05 00:53:10 +0800

    modified:   Crooptia/code/support.py 	modified:   Crooptia/code/__pycache__/level.cpython-311.pyc 	modified:   Crooptia/code/__pycache__/player.cpython-311.pyc 	modified:   Crooptia/code/player.py 	Crooptia/code/__pycache__/support.cpython-311.pyc 	Crooptia/code/__pycache__/timer.cpython-311.pyc 	Crooptia/code/timer.py 	Crooptia/data/

--- Crooptia/code/support.py
+++ Crooptia/code/support.py
@@ -6,9 +6,9 @@ def import_folder(path):
 
 	for _, __, img_files in walk(path):
 		for image in img_files:
-			print(image)
-			# full_path = path + '/' + image
-			# image_surf = pygame.image.load(full_path).convert_alpha()
-			# surface_list.append(image_surf)
+			
+			full_path = path + '/' + image
+			image_surf = pygame.image.load(full_path).convert_alpha()
+			surface_list.append(image_surf)
 
 	return surface_list
