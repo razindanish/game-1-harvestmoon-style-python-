f9ac2c559fcaef93d6f413275a10685beb675209
razindanish <54845743+razindanish@users.noreply.github.com>
2023-04-04 23:43:05 +0800

    modified:   Crooptia/Level.py 	modified:   Crooptia/Main.py 	modified:   Crooptia/Player.py 	modified:   Crooptia/__pycache__/Level.cpython-311.pyc 	modified:   Crooptia/__pycache__/Settings.cpython-311.pyc

--- Crooptia/Level.py
+++ Crooptia/Level.py
@@ -1,18 +1,20 @@
 import pygame 
-from Settings import *
-from Player import Player
+from settings import *
+from player import Player
 
 class Level:
 	def __init__(self):
-		
+
+		# get the display surface
 		self.display_surface = pygame.display.get_surface()
-		
+
+		# sprite groups
 		self.all_sprites = pygame.sprite.Group()
 
 		self.setup()
 
 	def setup(self):
-		self.Player = Player((640,360), self.all_sprites)
+		self.player = Player((640,360), self.all_sprites)
 
 	def run(self,dt):
 		self.display_surface.fill('black')
--- Crooptia/Main.py
+++ Crooptia/Main.py
@@ -1,28 +1,26 @@
-import pygame,sys
-from Settings import *
-from Level import Level
+import pygame, sys
+from settings import *
+from level import Level
 
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-        pygame.display.set_caption('Crooptia')
-        self.clock = pygame.time.Clock()
-        self.level = Level()
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		pygame.display.set_caption('Sprout land')
+		self.clock = pygame.time.Clock()
+		self.level = Level()
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
   
-            dt = self.clock.tick() / 1000
-            self.level.run(dt)
-            pygame.display.update()
-            
+			dt = self.clock.tick() / 1000
+			self.level.run(dt)
+			pygame.display.update()
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
-                     
+	game = Game()
+	game.run()
