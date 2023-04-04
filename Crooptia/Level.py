c2fb06a12d77ead57f4ad5655f8636f754640646
razindanish <54845743+razindanish@users.noreply.github.com>
2023-04-04 23:38:29 +0800

    new file:   Crooptia/Level.py new file:   Crooptia/Player.py new file:   Crooptia/Settings.py 	Crooptia/Main.py 	Crooptia/__pycache__/

---
+++ Crooptia/Level.py
@@ -0,0 +1,20 @@
+import pygame 
+from Settings import *
+from Player import Player
+
+class Level:
+	def __init__(self):
+		
+		self.display_surface = pygame.display.get_surface()
+		
+		self.all_sprites = pygame.sprite.Group()
+
+		self.setup()
+
+	def setup(self):
+		self.Player = Player((640,360), self.all_sprites)
+
+	def run(self,dt):
+		self.display_surface.fill('black')
+		self.all_sprites.draw(self.display_surface)
+		self.all_sprites.update(dt)
